c3d88f3a329411c0b03baaf33f39872b2daded1e
muhrusdi <muhammadrusdiid@gmail.com>
2025-07-09 21:58:56 +0700

    chore: update components

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   IconCamera,
   IconChartBar,
@@ -15,12 +15,12 @@ import {
   IconSearch,
   IconSettings,
   IconUsers,
-} from "@tabler/icons-react"
+} from "@tabler/icons-react";
 
-import { NavDocuments } from "~/components/nav-documents"
-import { NavMain } from "~/components/nav-main"
-import { NavSecondary } from "~/components/nav-secondary"
-import { NavUser } from "~/components/nav-user"
+import { NavDocuments } from "~/components/nav-documents";
+import { NavMain } from "~/components/nav-main";
+import { NavSecondary } from "~/components/nav-secondary";
+import { NavUser } from "~/components/nav-user";
 import {
   Sidebar,
   SidebarContent,
@@ -29,7 +29,7 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "~/components/ui/sidebar"
+} from "~/components/ui/sidebar";
 
 const data = {
   user: {
@@ -40,12 +40,12 @@ const data = {
   navMain: [
     {
       title: "Dashboard",
-      url: "#",
+      url: "/",
       icon: IconDashboard,
     },
     {
-      title: "Lifecycle",
-      url: "#",
+      title: "Deferred",
+      url: "/deferred",
       icon: IconListDetails,
     },
     {
@@ -146,7 +146,7 @@ const data = {
       icon: IconFileWord,
     },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -160,7 +160,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold">TSS</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -175,5 +175,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavUser user={data.user} />
       </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,29 +8,19 @@ import { postLoader } from "~/utils/posts";
 
 export const Route = createFileRoute("/")({
   component: Home,
-  // loaderDeps: ({ search: { last } }) => ({ last }),
   loader: async () => {
     const deferredPost = postLoader();
 
     return { deferredPost, data: _data, chartData: chartData };
   },
-  gcTime: 0,
-  // Only reload the route when the user navigates to it or when deps change
-  shouldReload: false,
 });
 
 function Home() {
-  const { deferredPost, data, chartData } = Route.useLoaderData();
+  const { data, chartData } = Route.useLoaderData();
 
   return (
     <div>
       <SiteHeader />
-      <Await promise={deferredPost} fallback={<div>Loading...</div>}>
-        {(data) => <div id="__id">{data.length}</div>}
-      </Await>
-      <Await promise={deferredPost} fallback={<div>Loading...</div>}>
-        {(data) => <div>{data.length}</div>}
-      </Await>
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
